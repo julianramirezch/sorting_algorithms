19b0600991811cd7da097a18276c6f40b045e26c
andergcp <andergcp@hotmail.com>
2020-06-15 16:36:31 +0000

    [FIX] Adding list validation and fixing betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,33 +1,40 @@
 #include "sort.h"
-
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers
+ * in ascending order using the Insertion sort algorithm
+ * @list: Double pointer to double linked list
+ */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *div, *aux, *tmp;
+	listint_t *div, *aux, *tmp = *list;
 
 	if (!*list)
 		return;
 
-	div = *list;
-	div = div->next;
-	while(div)
+	if (tmp->next)
 	{
-		aux = div;
+		div = *list;
 		div = div->next;
-		while(aux->prev && aux->prev->n > aux->n)
+		while (div)
 		{
-			tmp = aux->prev;
-			aux->prev = tmp->prev;
-			if(aux->prev)
-				aux->prev->next = aux;
-			tmp->next = aux->next;
-			if(tmp->next)
-				tmp->next->prev = tmp;
-			tmp->prev = aux;
-			aux->next = tmp;
-			if (!aux->prev)
-				*list = aux;
-			print_list(*list);
-		}
+			aux = div;
+			div = div->next;
+			while (aux->prev && aux->prev->n > aux->n)
+			{
+				tmp = aux->prev;
+				aux->prev = tmp->prev;
+				if (aux->prev)
+					aux->prev->next = aux;
+				tmp->next = aux->next;
+				if (tmp->next)
+					tmp->next->prev = tmp;
+				tmp->prev = aux;
+				aux->next = tmp;
+				if (!aux->prev)
+					*list = aux;
+				print_list(*list);
+			}
 
+		}
 	}
 }
