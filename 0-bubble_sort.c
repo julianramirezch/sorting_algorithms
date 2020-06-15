83b2076db6b163f30f8e5c61645db30f56abf52c
julianramirezch <julianramirezch1@gmail.com>
2020-06-15 10:55:57 -0500

    [add]: Bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,20 +1,24 @@
 #include "sort.h"
 
 /**
- * 
- * 
+ * bubble_sort - function that sorts an array using the Bubble sort algorithm
+ * @array: Array
+ * @size: Array size
  */
 
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
+	if (size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
-			{	
+			{
 				swap(&array[j], &array[j + 1]);
 				print_array(array, size);
 			}
