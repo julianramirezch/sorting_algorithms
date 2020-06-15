ae239d24643b36839766aca7aa0422cccf4ea3ca
andergcp <andergcp@hotmail.com>
2020-06-15 16:19:44 +0000

    [FIX] Fixing assignations

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -27,9 +27,10 @@ void swap(int *a, int *b)
 int partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
-	int i = (low - 1);
+	int i = (low - 1), j;
+	
 
-	for (int j = low; j <= high - 1; j++)
+	for (j = low; j <= high - 1; j++)
 	{
 		if (array[j] <= pivot)
 		{
