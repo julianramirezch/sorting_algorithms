311fb03cd4742454afbe5d9ed04d71f0429f5128
julianramirezch <julianramirezch1@gmail.com>
2020-06-16 12:29:51 -0500

    [add]: New Prototype

--- sort.h
+++ sort.h
@@ -25,6 +25,7 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void quick_sort2(int *array, int low, int high, size_t size);
+void cocktail_sort_list(listint_t **list);
 
 /* AUX FUNCTIONS */
 void swap(int *a, int *b);
