cbe787d0a02a45817e07be83bfa62b4f6727e5a2
andergcp <andergcp@hotmail.com>
2020-06-17 16:59:31 +0000

    [FIX] Adding validations of array

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -10,6 +10,9 @@ void shell_sort(int *array, size_t size)
 	size_t gap = 1, i, cmp;
 	int temp;
 
+/*validations*/
+	if (size == 0 || size == 1)
+		return;
 /*Finds the max gap*/
 	while (gap < (size / 3))
 		gap = gap * 3 + 1;
